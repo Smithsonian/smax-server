9e0dbc67246c729494412f1899f52a4f3870e292
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2021-11-04 17:48:08 -0400

    Attempted to fix some LUA errors ("unexpected end statement")

--- redis_config_files/lua/HMSetWithMeta.lua
+++ redis_config_files/lua/HMSetWithMeta.lua
@@ -2,7 +2,7 @@
 redis.replicate_commands()
 
 -- keys: [1] Hash table to add values to
--- arguments: 1:origin {2:field1 3:value1 4:type1 5:dim1 } { 6:field2 ... } ... 
+-- arguments: 1:origin {2:field1 3:value1 4:type1 5:dim1 } { 6:field2 ... } ...
 -- returns the result of the HMSET call for the {field,value} pairs
 local table = KEYS[1]   -- the name of the hash table
 local origin = ARGV[1]
@@ -10,7 +10,6 @@ local origin = ARGV[1]
 -- Timestamp
 local time = redis.call('time')
 local timestamp = time[1].."."..string.format("%06d", time[2])
-
 
 -- arrays / tables we'll need
 local ids = {}          -- composited meta IDs for all fields to be set
@@ -26,18 +25,17 @@ local serials = {}      -- {field,serial} pairs
 local leadArgs = 1
 local N = (#ARGV - leadArgs) / 4     -- Number of fields
 
--- A trailing 0 or non-zero value may explicitly select 
+-- A trailing 0 or non-zero value may explicitly select
 -- whether or not we want to notify parent structures
 local notifyParents = 'T'
 if #ARGV > leadArgs + 4 * N then
   notifyParents = ARGV[leadArgs + 4 * N + 1];
 end
 
-
-for k=1,N do 
+for k=1,N do
  local i = leadArgs + 4*k-3     -- i is the original ARGV index
  local j = 2 * k - 1            -- j is the {key,value} pairs list index
- 
+
  local field = ARGV[i]          -- field name
  local id = table..':'..field   -- the composited meta id
 
@@ -55,7 +53,7 @@ for k=1,N do
  types[j] = ARGV[i+2]       -- type
  dims[j] = ARGV[i+3]        -- dim
  timestamps[j] = timestamp
- origins[j] = origin 
+ origins[j] = origin
 end
 
 local result = redis.call('hmset', table, unpack(entries))
@@ -64,42 +62,55 @@ redis.call('hmset', '<dims>', unpack(dims))
 redis.call('hmset', '<timestamps>', unpack(timestamps))
 redis.call('hmset', '<origins>', unpack(origins))
 
--- Bulk update of the serial counters 
+-- Bulk update of the serial counters
 local counts = redis.call('hmget', '<writes>', unpack(ids))
-for i,ser in pairs(counts) do 
+for i,ser in pairs(counts) do
  if ser then
-  serials[2*i] = tonumber(ser) + 1 
+  serials[2*i] = tonumber(ser) + 1
  else
   serials[2*i] = 1
  end
-end 
+end
 redis.call('hmset', '<writes>', unpack(serials))
+
+local isExternalRMUpdate = false
+ local target = table:sub(4)
+ if origin:sub(1, target:len()) ~= target then
+   isExternalRMUpdate = true
+ end
 
 -- Send notification of this update
 for i,id in pairs(ids) do
  redis.call('publish', 'smax:'..id, origin)
-end
 
+ -- For RM updates coming from outside the targeted antenna send a notification
+ -- to the antenna's RM connector, including the data
+ if isExternalRMUpdate then
+  redis.call('publish', table, field .. "=" .. value)
+ end
+end
 
 -- Add/uppdate the parent hierachy as needed
 local parent = ''
+local newparent = 1
 for child in table:gmatch('[^:]+') do
-  if parent == '' then
-    parent = child
-  else
-    local id = parent..':'..child
-    redis.call('hset', parent, child, id);
+ if parent == '' then
+  parent = child
+ else
+  local id = parent..':'..child
+
+  if newparent == 1 then
+   newparent = redis.call('hset', parent, child, id)
+   if newparent == 1 then
     redis.call('hset', '<types>', id, 'struct')
     redis.call('hset', '<dims>', id, '1')
-    redis.call('hset', '<timestamps>', id, timestamp)
-    redis.call('hset', '<origins>', id, origin)
-    redis.call('hincrby', '<writes>', id, '1')
-    --if notifyParents == 'T' then
-    --  redis.call('publish', 'smax:'..id, origin)
-    --end
-	  parent = id
+   end
   end
+
+  redis.call('hset', '<timestamps>', id, timestamp)
+
+	parent = id
+ end
 end
 
 return result
-
--- redis_config_files/lua/HSetWithMeta.lua
+++ redis_config_files/lua/HSetWithMeta.lua
@@ -27,27 +27,35 @@ redis.call('hincrby', '<writes>', id, '1')
 -- Send notification for the table update
 redis.call('publish', 'smax:'..id, origin)
  
---Add/update the parent hierachy as needed
+-- For RM updates coming from outside the targeted antenna send a notification
+-- to the antenna's RM connector, including the data
+if table:sub(1, 3) == "RM:" then
+ local target = table:sub(4)
+ if origin:sub(1, target:len()) ~= target then
+  redis.call('publish', table, field .. "=" .. value)
+ end
+end
+ 
+-- Add/update the parent hierachy as needed
 local parent = ''
+local newparent = 1
 for child in table:gmatch('[^:]+') do
-  if parent == '' then
-    parent = child
-  else
-    id = parent..':'..child
-    redis.call('hset', parent, child, id);
+ if parent == '' then
+  parent = child
+ else
+  id = parent..':'..child
+  if newparent == 1 then
+   newparent = redis.call('hset', parent, child, id)
+   if newparent == 1 then 
     redis.call('hset', '<types>', id, 'struct')
     redis.call('hset', '<dims>', id, '1')
-    redis.call('hset', '<timestamps>', id, timestamp)
-    redis.call('hset', '<origins>', id, origin)
-    redis.call('hincrby', '<writes>', id, '1')
-
-    --redis.call('publish', 'smax:'..id, origin)
-
-    parent = id
+   end
   end
+    
+  redis.call('hset', '<timestamps>', id, timestamp)
+
+  parent = id
+ end
 end
 
 return result
-
-
-
